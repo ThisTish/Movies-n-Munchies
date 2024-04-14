955432f879f7bfc1b1e8ec14d38460a99663daa9
Tish <tishthegoat@gmail.com>
2024-04-14 07:08:25 -0600

    populate selectedMovieModal-mostly done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,38 @@
 const searchBtnEl = $('#search-button')
+//* for search modal
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+	modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+	modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
+
+// todo tie to searchmodal
+$('#search-form').on('submit', function(event){
+	event.preventDefault()
+	const searchInputEl = $('#search-input').val()
+	fetchMovieTitleApi(searchInputEl)
+})
 
 // *function for movie title search
 function fetchMovieTitleApi(search){
@@ -20,17 +54,12 @@ function fetchMovieTitleApi(search){
 	.catch(error => {
 		console.error('Fetch error:', error)
 	})
-
+	
 }
 
-$('#search-form').on('submit', function(event){
-	event.preventDefault()
-	const searchInputEl = $('#search-input').val()
-	fetchMovieTitleApi(searchInputEl)
-})
 
 // *function for random movie
-function fetchRandomMovie(random){
+function fetchRandomMovie(){
 	const apiKey = "05ee849ca5bf0c7ca64d3561ba1aa9b8"
 	const randomPage = Math.floor(Math.random() * 500) + 1//might need to change number-only goes to 500 
 	const randomMovieApi = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=en-US&region=United%20States&api_key=${apiKey}&page=${randomPage}`
@@ -53,45 +82,13 @@ function fetchRandomMovie(random){
 
 }
 
-// function for random button fetch
+// *function for random button fetch
 $('#random-button').on('click', function(event){
 	event.preventDefault()
 	fetchRandomMovie()
 })
 
-
-
-
-//* for search modal
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
-// function for populating movie results area
+// *function for populating movie results area
 // todo add poster to button, title on top or below poster?
 // todo create area when (random)btn is clicked
 function displayMovieResults(page){
@@ -117,6 +114,7 @@ function displayMovieResults(page){
 	}
 }
 
+// *function to search by id to get selectedMovieModal details
 $(document).on('click','#selectMovieBtn', function() {
 	const movieId = $(this).attr('data-movie-id')
 	console.log(movieId)
@@ -134,30 +132,92 @@ $(document).on('click','#selectMovieBtn', function() {
 		})
 		.then(movie => {
 			console.log(movie)
-			// displayMovieResults(page)
+			displaySelectedMovie(movie)
 		})
 		.catch(error => {
 			console.error('Fetch error:', error)
 		});
-	
 	}
 	fetchMovieId()
+})
+
+// *function to display movie details in selectedMovieModal
+// todo clear appropriate info before repopulating.
+// todo figure out poster/backdrop
+function displaySelectedMovie(movie){
+	const movieModal = $('#movieModal')
+	const dynamicElements = $('<div>')
+	dynamicElements.addClass('p-3')
+	dynamicElements.empty()
+
+	const movieModalHeader = $('<header>')
+
+	const movieTitle = $('<h2>')
+	movieTitle.addClass('text-pink')
+	movieTitle.text(movie.title) 
+
+	const close = $('<span>')//might add back to html
+	close.addClass('close absolute text-white top-0 right-0 p-4 cursor-pointer')
+	close.html('&times;')
+
+	// const poster = $('<img>')
+	// poster.addClass('w-10 h-auto')
+	// poster.attr('src', movie.poster_path)
+
+	console.log(movie.poster_path)//poster--how to get to show? maybe backdrop_path?
+	
+	const movieModalDetails = $('<div>')
+
+	if(movie.tagline){
+		const tagline = $('<p>')
+		tagline.addClass('text-green')
+		tagline.text(movie.tagline)
+		movieModalDetails.append(tagline)
+	}
+
+	
+	const date = movie.release_date
+	const justYear = date.split('-')[0]
+	const year = $('<p>')
+	year.addClass('text-purple')
+	year.text(`${justYear}`)
+
+	const overview = $('<p>')
+	overview.addClass('tracking-tight')
+	overview.text(movie.overview)
+
+	const rating = $('<p>')
+	rating.addClass('text-orange')
+	rating.text(movie.vote_average)//could find star rating image that goes with this, but that's a whole other function for a whole other day.
+
+	const genre = $('<p>')
+	genre.addClass('font-bold')
+	genre.text(movie.genres[0].name)//could add more IF they have more...later
+
+	const runtime = $('<p>')
+	runtime.addClass('text-sm')
+	runtime.text(`${movie.runtime} min`)
+
+	const homepage = $('<a>')
+	homepage.addClass('text-center')
+	homepage.attr('href', movie.homepage)
+	homepage.text(movie.homepage)//could make the image the anchor...maybe
+	
+	movieModalDetails.append(year, overview, rating, genre, runtime, homepage)
+	movieModalHeader.append(movieTitle, close)
+	dynamicElements.append(movieModalHeader, movieModalDetails)
+	movieModal.prepend(dynamicElements)
+	
 	selectedMovieModal.show()
+	
 }
-)
 
-
-//* Selected Movie Modal Functions
-// todo selected movie modal. still need to populate
-// displaySelectedMovie()
-console.log()
-
-
+//* Selected Movie Modal Button Functions
+// todo make these one liners
 const selectedMovieModal = $('#movieModal')
 const goBackBtn = $('#go-back')
 const closeBtn = $('.close')
 const saveForLaterBtn = $('#save-for-later')
-// todo make these one liners
 goBackBtn.on('click', () =>{
 	selectedMovieModal.hide()
 })
@@ -170,6 +230,12 @@ saveForLaterBtn.on('click', () =>{
 	// setLocalStorage() not done
 	selectedMovieModal.hide()
 	
+})
+
+$(document).click(function(event){
+	if(!selectedMovieModal.is(event.target) && selectedMovieModal.has(event.target).length === 0){
+		selectedMovieModal.hide()
+	}
 })
 
 
